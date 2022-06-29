e2a1cfd979d71bd3bd7f56482bcabf48d7771397
Nic S <100664187+skelekon@users.noreply.github.com>
2022-06-29 20:54:21 +0930

    Fixed issue with linked Powers

--- run_all.bat
+++ run_all.bat
@@ -1,8 +1,8 @@
 :: This will create three Fantasy Grounds modules
 :: 4E Monsters contains all NPCs
-:: 4E Powers contains all Races, Classes, Feats and Powers
+:: 4E PC Options contains all Races, Classes, Feats and Powers
 :: 4E Items contains all mundane & magical items, all Rituals and all Alchemical Items
 
 module_maker.exe --filename 4E_Monsters --library "4E Monsters" -n -t
-module_maker.exe --filename 4E_PCs --library "4E PCs" -r -c -f -p -b
+module_maker.exe --filename 4E_PC_Options --library "4E PC Options" -r -c -f -p -b
 module_maker.exe --filename 4E_Items --library "4E Items" -a -u -m -i -t
